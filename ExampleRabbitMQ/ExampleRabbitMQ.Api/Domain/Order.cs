5ce4ec30a5c539f03ecc1cc5dc0d281100115183
Samuel Paiva Carneiro <sampaiva97@gmail.com>
2020-09-11 16:02:16 -0300

    initial commit

---
+++ ExampleRabbitMQ/ExampleRabbitMQ.Api/Domain/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleRabbitMQ.Api.Domain
+{
+    public sealed class Order
+    {
+        public int OrderId { get; set; }
+        public string ItemName { get; set; }
+        public float Price { get; set; }
+    }
+}
